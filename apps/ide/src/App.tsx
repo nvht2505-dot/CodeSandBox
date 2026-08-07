c16b7087ef49e09233a2bd6bef09fa387beaf2c1
Your Name <you@example.com>
2026-08-07 23:32:33 +0700

    feat(editor): integrate monaco editor

--- apps/ide/src/App.tsx
+++ apps/ide/src/App.tsx
@@ -1,5 +1,6 @@
 import Explorer from "./components/Explorer";
 import AIAgent from "./components/AIAgent";
+import CodeEditor from "./components/Editor";
 
 export default function App() {
   return (
@@ -17,21 +18,29 @@ export default function App() {
 
       <div className="flex flex-1">
 
-        <aside className="w-72 border-r border-zinc-800">
+        <aside className="w-64 border-r border-zinc-800">
           <Explorer />
         </aside>
 
-        <main className="flex-1 border-r border-zinc-800">
-          <AIAgent />
-        </main>
+        <section className="flex-1 flex flex-col">
 
-        <aside className="w-[420px] p-4">
+          <div className="h-1/2 border-b border-zinc-800">
+            <CodeEditor />
+          </div>
+
+          <div className="h-1/2">
+            <AIAgent />
+          </div>
+
+        </section>
+
+        <aside className="w-[420px] border-l border-zinc-800 p-4">
           Live Preview
         </aside>
 
       </div>
 
-      <footer className="h-44 border-t border-zinc-800 p-4">
+      <footer className="h-40 border-t border-zinc-800 p-4">
         Terminal
       </footer>
 
